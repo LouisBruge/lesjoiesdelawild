bf6040df5d653ee102365d181c73e8044530b4c1
Dine16 <amandine.claude6@gmail.com>
2018-03-06 18:19:54 +0100

    add header.php

---
+++ src/html/header.php
@@ -0,0 +1,19 @@
+        <header>
+            <h1>L'ambiance à la Wild</h1>
+        <!-- Navbar -->
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">
+                            <img src="http://via.placeholder.com/70x70" alt="logo">
+                        </a>
+                    </div>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a id="top5" href="top5.php">Top 5</a></li>
+                        <li><a href="wildor">Le Wild d'or</a></li>
+                        <li><a id="contact" href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+        <!-- End navbar -->
+        </header>
