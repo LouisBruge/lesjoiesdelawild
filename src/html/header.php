f5f5a40fefd32b1a576351483ccc09a42537b16d
Dine16 <amandine.claude6@gmail.com>
2018-03-06 18:33:15 +0100

    add burger menu code in header

--- src/html/header.php
+++ src/html/header.php
@@ -4,10 +4,21 @@
             <nav class="navbar navbar-default">
                 <div class="container-fluid">
                     <div class="navbar-header">
+                        <!-- Burger menu -->
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" id="bs-example-navbar-collapse-1 aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <!-- End burger menu -->
+                        <!-- Logo -->
                         <a class="navbar-brand" href="#">
                             <img src="http://via.placeholder.com/70x70" alt="logo">
                         </a>
                     </div>
+                        <!-- End logo -->
+                    
                     <ul class="nav navbar-nav navbar-right">
                         <li><a id="top5" href="top5.php">Top 5</a></li>
                         <li><a href="wildor">Le Wild d'or</a></li>
