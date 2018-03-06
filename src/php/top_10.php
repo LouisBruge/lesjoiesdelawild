7c4cd5946aeb8a95a5d92d1b94fad1c44a7a73dd
Louis Bruge <louis.bruge@gmail.com>
2018-03-06 16:31:31 +0100

    Create the structure html of the top_10.php file

---
+++ src/php/top_10.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Les cinq commandements! Pas 10 pas flemme...</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    </body>
+</html>
