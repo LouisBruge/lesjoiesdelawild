1176c70d6bf218658320ec2448fb1c45daf3d8e5
Louis Bruge <louis.bruge@gmail.com>
2018-03-07 12:41:53 +0100

    Correction of the link to the top_10 page and to the index page

--- src/html/header.php
+++ src/html/header.php
@@ -12,15 +12,15 @@
                         </button>
                         <!-- End burger menu -->
                         <!-- Logo -->
-                        <a class="navbar-brand" href="#">
+                        <a class="navbar-brand" href="/public/index.php">
                             <img src="http://via.placeholder.com/70x70" alt="logo">
                         </a>
                     </div>
                         <!-- End logo -->
                     <div class="collapse navbar-collapse" id="top-navbar">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a id="top5" href="top5.php">Top 5</a></li>
-                        <li><a href="wildor">Le Wild d'or</a></li>
+                        <li><a id="top5" href="/src/php/top_10.php">Top 5</a></li>
+                        <li><a id="wildor" href="wildor">Le Wild d'or</a></li>
                         <li><a id="contact" href="#">Contact</a></li>
                     </ul>
                     </div>
