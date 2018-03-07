0f3ba4133161574de587a1ce1606bce78e057c1e
Louis Bruge <louis.bruge@gmail.com>
2018-03-07 21:30:15 +0100

    Remove useless code inside the header file, like head tags and links to the JS library (Jquery and Bootstrap)

--- src/html/header.php
+++ src/html/header.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Les joies de la Wild</title>
-        <link href="/lesjoiesdelawild/src/js/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-        <link rel="stylesheet" href="style.css">
-</head>
 
         <header>
         <!-- Navbar -->
@@ -23,7 +13,7 @@
                         <!-- End burger menu -->
                         <!-- Logo -->
                         <a class="navbar-brand" href="/public/index.php">
-                            <img class="logo" src="/lesjoiesdelawild/public/assets/img/navbarlogo.png" alt="logo">
+                            <img class="logo" src="/public/assets/img/navbarlogo.png" alt="logo">
                                 <div class="titre"><h1>Les joies de la Wild</h1>
                                 </div>
                         </a>
@@ -41,8 +31,3 @@
         <!-- End navbar -->
         </header>
 
-    <body>
-        <script src="/lesjoiesdelawild/src/jquery-3.3.1.js"></script>
-        <script src="/lesjoiesdelawild/src/js/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-    </body>
-</html>
