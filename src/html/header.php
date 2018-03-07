e0a8f2bb83a7d16303ac2113dc5c864d8618d866
Dine16 <amandine.claude6@gmail.com>
2018-03-07 09:29:50 +0100

    new change in the file for the burger menu

--- src/html/header.php
+++ src/html/header.php
@@ -1,11 +1,10 @@
         <header>
-            <h1>L'ambiance à la Wild</h1>
         <!-- Navbar -->
             <nav class="navbar navbar-default">
                 <div class="container-fluid">
                     <div class="navbar-header">
                         <!-- Burger menu -->
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" id="bs-example-navbar-collapse-1 aria-expanded="false">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar" aria-expanded="false">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
@@ -18,12 +17,13 @@
                         </a>
                     </div>
                         <!-- End logo -->
-                    
+                    <div class="collapse navbar-collapse" id="top-navbar">
                     <ul class="nav navbar-nav navbar-right">
                         <li><a id="top5" href="top5.php">Top 5</a></li>
                         <li><a href="wildor">Le Wild d'or</a></li>
                         <li><a id="contact" href="#">Contact</a></li>
                     </ul>
+                    </div>
                 </div>
             </nav>
         <!-- End navbar -->
