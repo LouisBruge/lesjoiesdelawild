6d5d7a3d7bec37556cca877cc9400c1713a10e89
Dine16 <amandine.claude6@gmail.com>
2018-03-13 19:32:02 +0100

    add second pic to carousel

--- public/index.php
+++ public/index.php
@@ -15,33 +15,21 @@
   <body>
     <?php include('../src/html/header.php');?>
   	<div class="container-fluid">
-
-  	<h1>L'ambiance à la Wild</h1>
-
-  	<div id="slider" class="carousel slide" data-ride="carousel">
-  		<ol class="carousel-indicators">
-  			<li data-target="#slider" data-slide-to="0" class="active"></li>
-    		<li data-target="#slider" data-slide-to="1"></li>
-  			<li data-target="#slider" data-slide-to="2"></li>
-		</ol>
+        <div id="slider" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#slider" data-slide-to="0" class="active"></li>
+                <li data-target="#slider" data-slide-to="1"></li>
+                <li data-target="#slider" data-slide-to="2"></li>
+            </ol>
 		<div class="carousel-inner">
 			<div class="item active">
 				<img src="/public/assets/img/beatles.jpg" alt="theBeatles">
-				<div class="carousel-caption">
-					<h3>C'est à Lille qu'on apprend le mieux</h3>
-				</div>
 			</div>
 			<div class="item">
 				<img src="http://via.placeholder.com/1500x500" alt="Image 2">
-				<div class="carousel-caption">
-					<h3>C'est à Lille qu'on rigole le plus</h3>
-				</div>
 			</div>
 			<div class="item">
 				<img src="http://via.placeholder.com/1500x500" alt="Image 2">
-				<div class="carousel-caption">
-					<h3>C'est à Lille que les gens sont les plus cools</h3>
-				</div>
 			</div>
 		</div>
 			<a class="left carousel-control" href="#slider" role="button" data-slide="prev">
@@ -52,7 +40,7 @@
 				<span class="glyphicon glyphicon-chevron-right"></span>
 				<span class="sr-only">Next</span>
 			</a>
-	</div>
+	    </div>
 	<div class="row">
 		<div class="col-md-5">
 			<img src="http://via.placeholder.com/140x140" class="img-responsive">
