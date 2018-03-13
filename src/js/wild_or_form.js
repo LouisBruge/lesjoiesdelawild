41f910df28aab28dbec015314e0b9d9f2bf6d48e
Louis Bruge <louis.bruge@gmail.com>
2018-03-13 16:21:39 +0100

    Use the ES6 syntax

--- src/js/wild_or_form.js
+++ src/js/wild_or_form.js
@@ -1,37 +1,38 @@
 // Function who return a new citation when the user submit a new one by the form
-document.getElementById("form_wild").addEventListener("submit", function() {
+document.getElementById('form_wild').addEventListener('submit', function() {
     // get the pseudo and the corpus by the form
-    var pseudo = document.getElementById("pseudo").value;
-    var corpus = document.getElementsByTagName("textarea")[0].value;
+    let pseudo = document.getElementById('pseudo').value
+    let corpus = document.getElementsByTagName('textarea')[0].value
 
-    console.log(pseudo);
+    console.log(pseudo)
     // check if the values are not null or indefined
-    if ( (! pseudo == "")  && (! corpus == "")) {
+    if ( (! pseudo == '')  && (! corpus == '')) {
 
         // create a new citation to the wild_book
-        var newElement = document.createElement("div");
-        newElement.id= "New_citation";
-        newElement.innerHTML = "<blockquote><p>" + corpus + "</p><small>by <cite>" + pseudo + "</cite></small></blockquote>";
+        let newElement = document.createElement('div')
+        newElement.id= 'New_citation'
+        newElement.innerHTML = `<blockquote><p>${corpus}</p><small>by <cite>${pseudo}</cite></small></blockquote>`
 
-        document.getElementById("citations").appendChild(newElement);
+        document.getElementById('citations').appendChild(newElement)
 
         // remove the form
-        var form = document.getElementById("form_wild");
+        let form = document.getElementById('form_wild')
         for (i = form.childNodes.length - 1; i > 0;  i--){
-            form.removeChild(form.childNodes[i]);
+            form.removeChild(form.childNodes[i])
         }
 
         // creation of the citation object
-        var citation = {
+        let citation = {
             pseudo: pseudo,
             corpus: corpus
-        };
+        }
 
-        citation = JSON.stringify(citation);
-        localStorage.setItem("test", citation);
+        citation = JSON.stringify(citation)
+        localStorage.setItem('test', citation)
        
     }
     else {
-        console.error("Error");
+        console.error('Error')
     }
         }) 
+
