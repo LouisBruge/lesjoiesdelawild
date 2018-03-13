8144982ffcd540089db24b491ae3ddc01c76c86a
Dine16 <amandine.claude6@gmail.com>
2018-03-13 20:18:59 +0100

    modifications made on file index.php

--- public/index.php
+++ public/index.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Meilleure Wild de France !</title>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="author" content="Louis Bruge, Lucas Poirier, Julien Lemaire et Amandine Claude">
+    <meta name="description" content="First project Wild Code School">
+    <meta name="keywords" content="First project">
+    <title>Bienvenue à Lille, futurs wilders !</title>
+<!-- Bootstrap CSS files -->
     <link rel="stylesheet" href="../src/js/bootstrap-3.3.7-dist/css/bootstrap.min.css"/>
+<!-- Personal CSS -->
     <link rel="stylesheet" type="text/css" href="../src/css/index.css"/>
     <link rel="stylesheet" type="text/css" href="../src/css/header.css"/>
     <link rel="stylesheet" type="text/css" href="../src/css/footer.css"/>
+<!-- Font -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-   
+<!-- Favicon -->
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
   </head>
 
   <body>
@@ -26,7 +40,7 @@
 				<img src="/public/assets/img/beatles.jpg" alt="theBeatles">
 			</div>
 			<div class="item">
-				<img src="http://via.placeholder.com/1500x500" alt="Image 2">
+				<img src="/public/assets/img/inside-school.jpg" alt="school">
 			</div>
 			<div class="item">
 				<img src="http://via.placeholder.com/1500x500" alt="Image 2">
@@ -41,51 +55,40 @@
 				<span class="sr-only">Next</span>
 			</a>
 	    </div>
+    </div>
 	<div class="row">
 		<div class="col-md-5">
-			<img src="http://via.placeholder.com/140x140" class="img-responsive">
+			<img src="/public/assets/img/slippers.jpg" alt="slippersMascott">
 			<p class="text-center"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam feugiat urna ac ullamcorper interdum. Sed mattis accumsan dolor, a cursus magna fermentum eu. Vestibulum vel tincidunt nisl, vitae</p>
-		</div>
-		<div class="col-md-2">
-			<div class="outer">
-  				
-			</div>
 		</div>
 		<div class="col-md-5">
 			<p class="text-center"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam feugiat urna ac ullamcorper interdum. Sed mattis accumsan dolor, a cursus magna fermentum eu. Vestibulum vel tincidunt nisl, vitae</p>
-			<img src="http://via.placeholder.com/140x140" class="img-responsive">
+			<img src="/public/assets/img/mascot.jpg" alt="mascotPostIt">
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-5">
-			<img src="http://via.placeholder.com/140x140" class="img-responsive">
+			<img src="/public/assets/img/alone.jpg" alt="alone">
 			<p class="text-center"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam feugiat urna ac ullamcorper interdum. Sed mattis accumsan dolor, a cursus magna fermentum eu. Vestibulum vel tincidunt nisl, vitae</p>
-		</div>
-		<div class="col-md-2">
-			<div class="outer">
-  				
-			</div>
 		</div>
 		<div class="col-md-5">
 			<p class="text-center"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam feugiat urna ac ullamcorper interdum. Sed mattis accumsan dolor, a cursus magna fermentum eu. Vestibulum vel tincidunt nisl, vitae</p>
-			<img src="http://via.placeholder.com/140x140" class="img-responsive">
+			<img src="/public/assets/img/alone.jpg" alt="alone">
 		</div>
 	</div>
     <section id="link_to_top_5">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xs-10 col-xs-offset-1 text-center">
-                    <p> Tu souhaites en découvrir d'avantage sur la <b>Wild Code School de Lille</b> ? <br />
-                        Sur son univers impitoyable? <br />
-                    </p>
+                    <p> Tu souhaites en savoir davantage sur l'univers impitoyable de la <b>Wild Code School de Lille</b> ?</p>
                 </div>
-                <a class="btn btn-orange col-xs-offset-1 col-xs-10" href="/src/php/top_10.php">Clique ici! </a>
+                <a class="btn btn-orange col-xs-offset-1 col-xs-10" href="/src/php/top_10.php">Clique ici !</a>
             </div>
         </div>
     </section>
 
         <?php include('../src/html/footer.php') ?>
-	</body>
-<script src="../src/js/jquery-3.3.1.js"></script>
-<script src="../src/js/bootstrap-3.3.7-dist/js/bootstrap.js"></script> 
+    <script src="../src/js/jquery-3.3.1.js"></script>
+    <script src="../src/js/bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+  </body>
 </html>
