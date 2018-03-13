3ce3fb25f8da9f79f9d613af37c8e1dd76a5cb02
Louis Bruge <louis.bruge@gmail.com>
2018-03-13 19:29:27 +0100

    Documente the weather functionnality

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,17 +1,27 @@
-function loadWeather () {
+function Weather () {
   const req = new XMLHttpRequest()
 
+    // send a new request to the openweathermap API
 	req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=Lille&units=metric&APPID=7b869320ac280f7052d6d53e160fdcc8', false)
 	req.send()
 
+    // receive the responce to the openweathermap API and translate it to a JSON object
 	let data = JSON.parse(req.response);
+
+    // Set the temperature
 	let temp = data.main.temp;
-	let state = data.weather[0].main;
+
+    // insert the temperature to the DOM
 	document.getElementById('temp').innerHTML = temp;
+
+    // Set the state 
+	let state = data.weather[0].main;
+
+    // Change the Lille's picture according to the weather 
 	if (state == 'Rain' || 'Clear' || 'Clouds') {
-	document.getElementById('Image').setAttribute('src', `assets/img/Lille_${state}.jpg`);
+	    document.getElementById('pictureOfLille').setAttribute('src', `assets/img/Lille_${state}.jpg`);
 	} else{
-	document.getElementById('Image').setAttribute('src', 'http://s3.amazonaws.com/theoatmeal-img/coloring_book/header.png');
+	    document.getElementById('pictureOfLille').setAttribute('src', 'http://s3.amazonaws.com/theoatmeal-img/coloring_book/header.png');
 	}
 
 }
