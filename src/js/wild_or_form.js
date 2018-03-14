552daf25e8fa1b88b9aa76f1781c2a2319b7b168
Louis Bruge <louis.bruge@gmail.com>
2018-03-14 19:29:14 +0100

    Create a JSON object and create a new function to print the first comment

--- src/js/wild_or_form.js
+++ src/js/wild_or_form.js
@@ -1,3 +1,38 @@
+// Create a array to conserve the comments
+let citations = [] 
+
+// Create a constructor for the comments
+function comment(pseudo, corpus) {
+    this.pseudo = pseudo
+    this.corpus = corpus
+}
+
+// Write the first comments
+let louisComment = new comment ('louis', 'Super formation, j\'ai eu la chance de former 22 noobs et de réussir avec succes mon CAPES développeur web!')
+
+let lucasComment = new comment ('Lucas', 'Lorem ipsum, bro! Et merci à Toto, Tutu et Tata pour leurs conseils précieux')
+
+let amandineComment = new comment ('Amandine', 'Enorme grace à cette formation j\'ai pu intégrer la maison Dior en tant que developpeur PHP, " JS is a useless language" ! Merci Gaetan !')
+
+let julienComment = new comment ('Julien', 'Malgré une super ambiance j\'ai quitté la wild après le projet 1,(Conflit interne avec Ama....e) ! Et j\'ai ouvert un foodtruck à Byron bay en Australie. See you there mates. PS : -10% pour les wilders!')
+
+// Insert the first comments inside the array citations
+citations.push(louisComment)
+citations.push(lucasComment)
+citations.push(amandineComment)
+citations.push(julienComment)
+    
+// Function to create a citation inside the wild_book
+function citationHTML (pseudo, corpus) {
+    let newElement = document.createElement('div')
+    newElement.innerHTML = `<blockquote><p>${corpus}</p><small>by <cite>${pseudo}</cite</small></blockquote>`
+    document.getElementById('citations').appendChild(newElement)
+}
+
+for (let i in citations ) {
+    citationHTML(citations[i].pseudo, citations[i].corpus)
+}
+
 // Function who return a new citation when the user submit a new one by the form
 document.getElementById('form_wild').addEventListener('submit', function() {
     // get the pseudo and the corpus by the form
@@ -8,27 +43,16 @@ document.getElementById('form_wild').addEventListener('submit', function() {
     // check if the values are not null or indefined
     if ( (! pseudo == '')  && (! corpus == '')) {
 
-        // create a new citation to the wild_book
-        let newElement = document.createElement('div')
-        newElement.id= 'New_citation'
-        newElement.innerHTML = `<blockquote><p>${corpus}</p><small>by <cite>${pseudo}</cite></small></blockquote>`
-
-        document.getElementById('citations').appendChild(newElement)
-
-        // remove the form
-        let form = document.getElementById('form_wild')
-        for (i = form.childNodes.length - 1; i > 0;  i--){
-            form.removeChild(form.childNodes[i])
-        }
+        citationHTML(pseudo, corpus)
 
         // creation of the citation object
-        let citation = {
-            pseudo: pseudo,
-            corpus: corpus
-        }
+        newComment = new comment(pseudo, corpus)
 
-        citation = JSON.stringify(citation)
-        localStorage.setItem('test', citation)
+        // add the new comment inside the citations
+        citations.push(newComment)
+
+        citation = JSON.stringify(newComment)
+        localStorage.setItem('test', newComment)
        
     }
     else {
