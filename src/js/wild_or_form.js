12cd41d03fc4f53b74b584b2e3628fd92b6be687
Louis Bruge <louis.bruge@gmail.com>
2018-03-14 20:47:29 +0100

    Create a function to insert new comments into the citations JSON object

--- src/js/wild_or_form.js
+++ src/js/wild_or_form.js
@@ -1,11 +1,27 @@
-// Create a array to conserve the comments
-let citations = [] 
+// Function to create a citation inside the wild_book
+function citationHTML (pseudo, corpus) {
+    let newElement = document.createElement('div')
+    newElement.innerHTML = `<blockquote><p>${corpus}</p><small>by <cite>${pseudo}</cite</small></blockquote>`
+    document.getElementById('citations').appendChild(newElement)
+}
+
+// Function to put a new comment inside a JSON object
+function insertCommentToJSON ( objectJSON, comment) {
+    objectJSON = JSON.parse(objectJSON)
+    objectJSON.push(comment)
+    objectJSON = JSON.stringify(objectJSON)
+    return objectJSON
+}
 
 // Create a constructor for the comments
 function comment(pseudo, corpus) {
     this.pseudo = pseudo
     this.corpus = corpus
 }
+
+// Create a array to conserve the comments
+let citations = [] 
+citations = JSON.stringify(citations)
 
 // Write the first comments
 let louisComment = new comment ('louis', 'Super formation, j\'ai eu la chance de former 22 noobs et de réussir avec succes mon CAPES développeur web!')
@@ -17,43 +33,41 @@ let amandineComment = new comment ('Amandine', 'Enorme grace à cette formation 
 let julienComment = new comment ('Julien', 'Malgré une super ambiance j\'ai quitté la wild après le projet 1,(Conflit interne avec Ama....e) ! Et j\'ai ouvert un foodtruck à Byron bay en Australie. See you there mates. PS : -10% pour les wilders!')
 
 // Insert the first comments inside the array citations
-citations.push(louisComment)
-citations.push(lucasComment)
-citations.push(amandineComment)
-citations.push(julienComment)
-    
-// Function to create a citation inside the wild_book
-function citationHTML (pseudo, corpus) {
-    let newElement = document.createElement('div')
-    newElement.innerHTML = `<blockquote><p>${corpus}</p><small>by <cite>${pseudo}</cite</small></blockquote>`
-    document.getElementById('citations').appendChild(newElement)
-}
+citations = insertCommentToJSON(citations, louisComment)
+citations = insertCommentToJSON(citations, lucasComment)
+citations = insertCommentToJSON(citations, amandineComment)
+citations = insertCommentToJSON(citations, julienComment)
 
+// Transform the citation into a Object
+citations = JSON.parse(citations)
+
+// Print the firsts comments
 for (let i in citations ) {
     citationHTML(citations[i].pseudo, citations[i].corpus)
 }
 
+// Retransform the citation objet into a JSON object
+citations = JSON.stringify(citations)
+
+
 // Function who return a new citation when the user submit a new one by the form
 document.getElementById('form_wild').addEventListener('submit', function() {
+
     // get the pseudo and the corpus by the form
     let pseudo = document.getElementById('pseudo').value
     let corpus = document.getElementsByTagName('textarea')[0].value
 
-    console.log(pseudo)
     // check if the values are not null or indefined
     if ( (! pseudo == '')  && (! corpus == '')) {
-
-        citationHTML(pseudo, corpus)
 
         // creation of the citation object
         newComment = new comment(pseudo, corpus)
 
-        // add the new comment inside the citations
-        citations.push(newComment)
+        // Print the citation inside the web page
+        citationHTML(newComment.pseudo, newComment.corpus)
 
-        citation = JSON.stringify(newComment)
-        localStorage.setItem('test', newComment)
-       
+        citations = insertCommentToJSON(citations, newComment)
+
     }
     else {
         console.error('Error')
